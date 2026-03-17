ba380b9f2a7697c22cba739a9700cd5baae32fd3
gkawanami <guilherme.kawanami@gmail.com>
2026-03-17 11:29:06 -0300

    Categoria
    Categoria da parte da home

---
+++ src/components/Categoria/Categoria.jsx
@@ -0,0 +1,15 @@
+function Categoria() {
+    return(
+        <Section>
+            <div>
+                <h3>Escolha o seu estilo de carro</h3>
+                <h2>Categorias em Alta</h2>
+            </div>
+            <div>
+                
+            </div>
+        </Section>
+    )
+}
+
+export default Categoria
