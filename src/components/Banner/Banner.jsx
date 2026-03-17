aa74e6529291bf0a88fbd50b722b05eb365ce89c
gkawanami <guilherme.kawanami@gmail.com>
2026-03-17 11:28:07 -0300

    Banner
    Desenvolvimento do banner que será colocado na home

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import css from './Banner.module.css'
+function Banner() {
+    return(
+        <section>
+            <div className={css.banner}>
+                <div className={css.texto}>
+                    <h1>A <span>Maior Garagem</span> de Carros Usados do <span>Senai</span></h1>
+                    <h2>Nossa equipe oferece uma ampla seleção de carros a pronta entrega.</h2>
+                    <Link className={css.button} to="/carros">Ver Carros</Link>
+                </div>
+                <div>
+                    <img src="/img/carro-banner.png" alt="Banner" />
+                </div>
+            </div>
+            <img src="/img/marcas.png" alt="marcas banner" />
+        </section>
+    )
+}
+
+export default Banner
