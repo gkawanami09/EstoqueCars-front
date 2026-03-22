9c1f3208fc2dedee96391350f2947155ceece909
gkawanami <guilherme.kawanami@gmail.com>
2026-03-22 20:34:27 -0300

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Header from "../components/Header/Header"
 import Banner from "../components/Banner/Banner"
 import Categoria from "../components/Categoria/Categoria"
+import CarrosHome from "../components/CarrosHome/CarrosHome"
 
 function Home() {
     return(
         <>
             <Header />
-            <Banner />
-            <Categoria />
+            <Banner span1="Maior Garagem" titulo="de Carros Usados do" span2="Senai" subtitulo="Nossa equipe oferece uma ampla seleção de carros a pronta entrega." buttonTo="/carros" buttonNome="ver carros"/>
+            <Categoria subtitulo="Escolha o seu estilo de carro" titulo="Categorias"/>
+            <CarrosHome subtitulo="Explore nossa coleção exclusiva" titulo="Diversos Modelos de " span="Carros"/>
         </>
     )
 }
