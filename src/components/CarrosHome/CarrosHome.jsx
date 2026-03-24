a6e5261f8303327ee67cec62a48848c9743bedf7
gkawanami <guilherme.kawanami@gmail.com>
2026-03-24 11:25:36 -0300

    Atualização Carro home

--- src/components/CarrosHome/CarrosHome.jsx
+++ src/components/CarrosHome/CarrosHome.jsx
@@ -1,5 +1,6 @@
 import css from "./CarrosHome.module.css"
 import CardCarrosHome from "../CardCarrosHome/CardCarrosHome"
+import ButtonLink from "../ButtonLink/ButtonLink"
 
 function CarrosHome({subtitulo, titulo, span}) {
     return (
@@ -8,13 +9,15 @@ function CarrosHome({subtitulo, titulo, span}) {
                 <h3>{subtitulo}</h3>
                 <h2>{titulo}<span>{span}</span></h2>
             </div>
-            <div>
+            <div className={css.cards}>
+                <CardCarrosHome />
                 <CardCarrosHome />
                 <CardCarrosHome />
                 <CardCarrosHome />
                 <CardCarrosHome />
                 <CardCarrosHome />
             </div>
+            <ButtonLink buttonTo="/carros" buttonNome="Explorar Carros"/>
         </section>
     )
 }
