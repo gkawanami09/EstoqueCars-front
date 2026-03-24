da862272a71a5f7787699eeabcf2869cdbf586cb
gkawanami <guilherme.kawanami@gmail.com>
2026-03-24 11:26:41 -0300

    Atualização Button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Header from "../components/Header/Header"
 import Banner from "../components/Banner/Banner"
 import Categoria from "../components/Categoria/Categoria"
 import CarrosHome from "../components/CarrosHome/CarrosHome"
+import Beneficio from "../components/Beneficio/Beneficio"
+import Etapas from "../components/Etapas/Etapas"
 
 function Home() {
     return(
@@ -10,6 +12,8 @@ function Home() {
             <Banner span1="Maior Garagem" titulo="de Carros Usados do" span2="Senai" subtitulo="Nossa equipe oferece uma ampla seleção de carros a pronta entrega." buttonTo="/carros" buttonNome="ver carros"/>
             <Categoria subtitulo="Escolha o seu estilo de carro" titulo="Categorias"/>
             <CarrosHome subtitulo="Explore nossa coleção exclusiva" titulo="Diversos Modelos de " span="Carros"/>
+            <Beneficio titulo1="Oferecemos uma" span1="ampla" titulo2="seleção de" span2="carros acessíveis" subtitulo="O HB20 é um usado acessível que chama atenção pelo equilíbrio entre economia, estilo e praticidade."/>
+            <Etapas subtitulo="Siga etapas simples" titulo1="Como" span="comprar" titulo2="seu carro"/>
         </>
     )
 }
