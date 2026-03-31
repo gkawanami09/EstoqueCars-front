3932da29132ff664fa8d6db76b83dc0b4907e81e
gkawanami <guilherme.kawanami@gmail.com>
2026-03-31 01:31:12 -0300

    Finalização da página home
    desenvolvimento do Faq, Contato e footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,30 @@ import Categoria from "../components/Categoria/Categoria"
 import CarrosHome from "../components/CarrosHome/CarrosHome"
 import Beneficio from "../components/Beneficio/Beneficio"
 import Etapas from "../components/Etapas/Etapas"
+import Faq from "../components/Faq/Faq"
+import Contato from "../components/Contato/Contato"
+import Footer from "../components/Footer/Footer"
 
 function Home() {
     return(
         <>
             <Header />
+
             <Banner span1="Maior Garagem" titulo="de Carros Usados do" span2="Senai" subtitulo="Nossa equipe oferece uma ampla seleção de carros a pronta entrega." buttonTo="/carros" buttonNome="ver carros"/>
+
             <Categoria subtitulo="Escolha o seu estilo de carro" titulo="Categorias"/>
+
             <CarrosHome subtitulo="Explore nossa coleção exclusiva" titulo="Diversos Modelos de " span="Carros"/>
+
             <Beneficio titulo1="Oferecemos uma" span1="ampla" titulo2="seleção de" span2="carros acessíveis" subtitulo="O HB20 é um usado acessível que chama atenção pelo equilíbrio entre economia, estilo e praticidade."/>
-            <Etapas subtitulo="Siga etapas simples" titulo1="Como" span="comprar" titulo2="seu carro"/>
+
+            <Etapas subtitulo="Siga etapas simples" titulo1="Como" span="Comprar" titulo2="seu Carro"/>
+
+            <Faq span="Dúvidas" titulo="Frequentes" subtitulo="FAQ"/>
+
+            <Contato />
+
+            <Footer />
         </>
     )
 }
