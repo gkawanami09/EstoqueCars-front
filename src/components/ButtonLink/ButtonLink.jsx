670052b61fbc77c5c314beba79d86132ab8fd75c
gkawanami <guilherme.kawanami@gmail.com>
2026-03-31 01:33:13 -0300

    Atualização da estética dos botões

--- src/components/ButtonLink/ButtonLink.jsx
+++ src/components/ButtonLink/ButtonLink.jsx
@@ -1,9 +1,11 @@
 import css from './ButtonLink.module.css'
 import { Link } from "react-router-dom"
 
-function Button({buttonTo, buttonNome}) {
+function Button({buttonTo, buttonNome, variant="preto", className=""}) {
+    const variantClass = css[variant] || ""
+    const classes = [css.button, variantClass, className].filter(Boolean).join(" ")
     return(
-        <Link className={css.button} to={buttonTo}>{buttonNome}</Link>
+        <Link className={classes} to={buttonTo}>{buttonNome}</Link>
     )
 }
 
