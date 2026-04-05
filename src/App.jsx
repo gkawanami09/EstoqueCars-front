6da38293ab3d69f160e67b5b74f44db24ace2f0a
gkawanami <guilherme.kawanami@gmail.com>
2026-04-04 23:09:07 -0300

    chore(app): configurar roteamento e integração base com API
    - Estruturado o App com rotas para Home, Cadastro, Login e Confirmar E-mail.
    - Centralizada a renderização de Header e Footer no App para evitar duplicidade nas páginas.
    - Parametrizada URL da API via prop no main para integração do frontend com backend.
    - Atualizada versão de react-router-dom e lockfile para consistência de dependências.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Header from './components/Header/Header'
+import Cadastro from "./pages/Cadastro"
+import Login from "./pages/Login"
+import ConfirmarEmail from "./pages/ConfirmarEmail"
+import Footer from './components/Footer/Footer'
 
-function App() {
+function App({ API }) {
   return (
-    <>
       <BrowserRouter>
-        <Home />
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro API={API}/>} />
+            <Route path="/login" element={<Login API={API}/>} />
+            <Route path="/confirmar-email" element={<ConfirmarEmail API={API} />} />
+
+            {/* <Route path="*" element={<Erro404 />} /> */}
+          </Routes>
+        <Footer />
       </BrowserRouter>
-    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App API="http://192.168.1.8:5000"/>
   </StrictMode>,
 )
