f2060377fee62c43ab4718ac31068f660cc98c95
perobelliisa <isagperobelli@gmail.com>
2026-04-28 08:51:39 -0300

    Responsividade
    Responsividade das páginas de cadastro de manutenção e edição de manutenção.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-    <App API="http://10.92.3.162:5000"/>
+    <App API="http://10.92.3.153:5000"/>
 
 
   </StrictMode>,
